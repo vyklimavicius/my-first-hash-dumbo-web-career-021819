d9cbbc2538e22fd828fc0b472e6352f8005ff5f3
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-02-02 01:30:30 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,7 +15,8 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
+  
+  shipping_manifest["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
